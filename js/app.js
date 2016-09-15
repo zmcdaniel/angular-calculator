fae74e8334e8343e2c2e2a22ac83feb978c2a28d
zmcdaniel <zmcdaniel@gmail.com>
2016-09-15 13:36:33 -0700

    Added ng-click stuff to all relevant buttons

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 console.log('app.js was loaded.');
+
+var calc = angular.module("SimpleCalc", []);
+
+calc.controller('HomeCtrl', ['$scope', function($scope) {
+    $scope.result = 0;
+
+    $scope.sevenButton = function() {
+        $scope.result = 7 + $scope.result;
+    }
+}]);
